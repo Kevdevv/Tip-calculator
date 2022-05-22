d4f9ec2854b1550bdb7b65f17d9d22f9f95bd257
Ramanantsoa Kevin <ramanantsoa.vlm@gmail.com>
2022-05-22 09:27:49 +0200

    ajout de tout les bouton % JS et ajout de condition pour ne pas avoir NaN si aucun champ rempli

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Bouton 5%
+
 function calcul5() {
     let bill = document.getElementById('bill')
     let customer = document.getElementById('people')
@@ -11,9 +13,122 @@ function calcul5() {
     let tipsPerPerson = tips / customer.value
     let totalPerPerson = (parseInt(bill.value) + tips) / customer.value
 
-    document.getElementById('tip-price').innerHTML = tipsPerPerson
-    document.getElementById('total-price').innerHTML = totalPerPerson
+    if (bill.value, customer.value == 0) {
+        document.getElementById('tip-price').innerHTML = '0€'
+        document.getElementById('total-price').innerHTML = '0€'
+    } else {
+        document.getElementById('tip-price').innerHTML = tipsPerPerson + ' €'
+        document.getElementById('total-price').innerHTML = totalPerPerson + ' €'
+    }
 }
 
 let button5 = document.getElementById('5')
 button5.addEventListener('click', calcul5)
+
+//Bouton 10%
+
+function calcul10() {
+    let bill = document.getElementById('bill')
+    let customer = document.getElementById('people')
+    let button10 = document.getElementById('10')
+
+    function amountTips() {
+        return (parseInt(button10.value) * parseInt(bill.value)) / 100
+    }
+
+    let tips = amountTips()
+    let tipsPerPerson = tips / customer.value
+    let totalPerPerson = (parseInt(bill.value) + tips) / customer.value
+
+    if (bill.value, customer.value == 0) {
+        document.getElementById('tip-price').innerHTML = '0€'
+        document.getElementById('total-price').innerHTML = '0€'
+    } else {
+        document.getElementById('tip-price').innerHTML = tipsPerPerson + ' €'
+        document.getElementById('total-price').innerHTML = totalPerPerson + ' €'
+    }
+}
+
+let button10 = document.getElementById('10')
+button10.addEventListener('click', calcul10)
+
+//Bouton 15%
+
+function calcul15() {
+    let bill = document.getElementById('bill')
+    let customer = document.getElementById('people')
+    let button15 = document.getElementById('15')
+
+    function amountTips() {
+        return (parseInt(button15.value) * parseInt(bill.value)) / 100
+    }
+
+    let tips = amountTips()
+    let tipsPerPerson = tips / customer.value
+    let totalPerPerson = (parseInt(bill.value) + tips) / customer.value
+
+    if (bill.value, customer.value == 0) {
+        document.getElementById('tip-price').innerHTML = '0€'
+        document.getElementById('total-price').innerHTML = '0€'
+    } else {
+        document.getElementById('tip-price').innerHTML = tipsPerPerson + ' €'
+        document.getElementById('total-price').innerHTML = totalPerPerson + ' €'
+    }
+}
+
+let button15 = document.getElementById('15')
+button15.addEventListener('click', calcul15)
+
+//Bouton 25%
+
+function calcul25() {
+    let bill = document.getElementById('bill')
+    let customer = document.getElementById('people')
+    let button25 = document.getElementById('25')
+
+    function amountTips() {
+        return (parseInt(button25.value) * parseInt(bill.value)) / 100
+    }
+
+    let tips = amountTips()
+    let tipsPerPerson = tips / customer.value
+    let totalPerPerson = (parseInt(bill.value) + tips) / customer.value
+
+    if (bill.value, customer.value == 0) {
+        document.getElementById('tip-price').innerHTML = '0€'
+        document.getElementById('total-price').innerHTML = '0€'
+    } else {
+        document.getElementById('tip-price').innerHTML = tipsPerPerson + ' €'
+        document.getElementById('total-price').innerHTML = totalPerPerson + ' €'
+    }
+}
+
+let button25 = document.getElementById('25')
+button25.addEventListener('click', calcul25)
+
+//Bouton 50% 
+
+function calcul50() {
+    let bill = document.getElementById('bill')
+    let customer = document.getElementById('people')
+    let button25 = document.getElementById('50')
+
+    function amountTips() {
+        return (parseInt(button50.value) * parseInt(bill.value)) / 100
+    }
+
+    let tips = amountTips()
+    let tipsPerPerson = tips / customer.value
+    let totalPerPerson = (parseInt(bill.value) + tips) / customer.value
+
+    if (bill.value, customer.value == 0) {
+        document.getElementById('tip-price').innerHTML = '0€'
+        document.getElementById('total-price').innerHTML = '0€'
+    } else {
+        document.getElementById('tip-price').innerHTML = tipsPerPerson + ' €'
+        document.getElementById('total-price').innerHTML = totalPerPerson + ' €'
+    }
+}
+
+let button50 = document.getElementById('50')
+button50.addEventListener('click', calcul50)
